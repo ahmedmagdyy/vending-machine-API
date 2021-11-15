ba252ff873815849ac0f0850e251b7ee538ba5e9
Ahmed Magdy <ahmedmagdy2016@gmail.com>
2021-11-15 09:22:41 +0200

    add typeorm config

---
+++ ormconfig.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.HOST,
+  port: process.env.PG_PORT,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  // synchronize: true,
+  migrationsTableName: 'db_migrations',
+  migrations: ['migration/*.js'],
+  logging: true,
+  cli: {
+    migrationsDir: 'migration',
+  },
+};
